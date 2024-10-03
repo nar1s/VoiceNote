507438e34886d185c0692363086695c6d66450cb
Pavel Kuznetsov <pavelakuz@gmail.com>
2024-10-03 05:02:35 +0300

    initial commit

---
+++ VoiceNote/VoiceNoteApp.swift
@@ -0,0 +1,18 @@
+//
+//  VoiceNoteApp.swift
+//  VoiceNote
+//
+//  Created by Pavel Kuznetsov on 02.10.2024.
+//
+
+
+import SwiftUI
+
+@main
+struct VoiceNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
